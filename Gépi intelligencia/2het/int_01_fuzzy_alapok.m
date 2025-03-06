e34ee32a82cea5bf22ba9efe8c00f141c993217c
py-snake <py-snake@gmx.com>
2025-03-06 13:46:37 +0100

    upload

--- Gépi intelligencia/2het/int_01_fuzzy_alapok.m
+++ Gépi intelligencia/2het/int_01_fuzzy_alapok.m
@@ -2,3 +2,5 @@
 
 % fuzzyLogicDesigner
 
+% neuroFuzzyDesigner
+
---
+++ Gépi intelligencia/3het/lab_03_jegyzet.m
@@ -0,0 +1,11 @@
+% amfis fügvényközelítés
+% excel táblázatban 10db pont
+% pl e az x-ediken, nem pontos értékkel hanem kb
+% neurális háló tanítása és a függvény kirajzolása
+% 11.pont függvényérték meghatározása neurális hálóval
+
+% kockázatkezelés
+% probléma meghatározása, legalább 6 paraméterrel
+% szétválaszható legyen két csoportra
+% két alrendszer összekapcsolása
+
